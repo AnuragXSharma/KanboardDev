76fa03da2ab35adb7ff2f03cbaca69b7671c7351
AnuragXSharma <anuragxsharma@rediffmail.com>
2018-01-22 19:30:48 -0500

    Ver1.1
    Published Version for Soft Launch

--- app/Template/config/about.php
+++ app/Template/config/about.php
@@ -2,72 +2,17 @@
     <h2><?= t('About Project Management Master') ?></h2>
 </div>
 <div class="panel">
-    <p> Project Management Master uses Kanboard concept to easily visualize flow of activities.
+    <p> Prime Project Master uses Kanboard concept to easily visualize flow of activities.
     It can be used for effectively controlling any process be it Projects, Education, Schools, Sales, Society Maintenance Management 
     and even govt. process. Though current template is developed keeping projects in mind.
     Its is developed based on open source and encourage anyone to reach out to me to collaborate and build further on it.
-    It can be adapted by SME for their project management either in-house or cloud based. Please visit us @ for your feedback and comments.
+    It can be adapted by SME for their project management either in-house or cloud based. Please visit us @ <a href="https://www.facebook.com/ProjectPrimeOffice/" target="_blank"> <i class="fa fa-handshake-o fa-fw"></i>ProjectPrimeOffie</a> for your feedback and comments.
     I am working on my next release and any feedback and help will be appreciated.
+    Please drop me an email <i class="fa fa-envelope fa-fw"></i> @ "headprimeconsulting@gmail.com" if you want to be part of prime community or help out in anyway.
     </p>
    
 </div>
 
-<div class="page-header">
-    <h2><?= t('Configuration') ?></h2>
-</div>
-<div class="panel">
-    <ul>
-        <li>
-            <?= t('Application version:') ?>
-            <strong><?= APP_VERSION ?></strong>
-        </li>
-        <li>
-            <?= t('PHP version:') ?>
-            <strong><?= PHP_VERSION ?></strong>
-        </li>
-        <li>
-            <?= t('Database driver:') ?>
-            <strong><?= DB_DRIVER ?></strong>
-        </li>
-        <li>
-            <?= t('Database version:') ?>
-            <strong><?= $this->text->e($db_version) ?></strong>
-        </li>
-        <li>
-            <?= t('Browser:') ?>
-            <strong><?= $this->text->e($user_agent) ?></strong>
-        </li>
-    </ul>
-</div>
 
-<?php if (DB_DRIVER === 'sqlite'): ?>
-    <div class="page-header">
-        <h2><?= t('Database') ?></h2>
-    </div>
-    <div class="panel">
-        <ul>
-            <li>
-                <?= t('Database size:') ?>
-                <strong><?= $this->text->bytes($db_size) ?></strong>
-            </li>
-            <li>
-                <?= $this->url->link(t('Download the database'), 'ConfigController', 'downloadDb', array(), true) ?>&nbsp;
-                <?= t('(Gzip compressed Sqlite file)') ?>
-            </li>
-            <li>
-                <?= $this->url->link(t('Upload the database'), 'ConfigController', 'uploadDb', array(), false, 'js-modal-medium') ?>
-            </li>
-            <li>
-                <?= $this->url->link(t('Optimize the database'), 'ConfigController', 'optimizeDb', array(), true) ?>&nbsp;
-                <?= t('(VACUUM command)') ?>
-            </li>
-        </ul>
-    </div>
-<?php endif ?>
+    
 
-<div class="page-header">
-    <h2><?= t('License') ?></h2>
-</div>
-<div class="panel">
-<?= nl2br(file_get_contents(ROOT_DIR.DIRECTORY_SEPARATOR.'LICENSE')) ?>
-</div>
